416c7b3f4fab40e05d0df449ac94a7bf2ac30d5c
DESKTOP-U5NIS8S\gabri <gaarag@floridauniversitaria.es>
2022-09-22 13:49:09 +0200

    Update ej2

--- PMDM/figuras/ej2/App.js
+++ PMDM/figuras/ej2/App.js
@@ -1,12 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Square } from './components/Figures.js'
+import { Square, Rectangle, Triangle, Circle, QuarterCircle } from './components/Figures.js'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Square style={styles.square}/>
       <Square style={styles.bigSquare}/>
+      <br></br>
+      <Rectangle style={styles.rectangleUp}/>
+      <View style={{ flexWrap: 'wrap', alignItems: 'flex-start', flexDirection: 'row',}}>
+        <Square style={styles.squareRight}/>
+        <Square style={styles.squareLeft}/>
+      </View>
+      <Rectangle style={styles.rectangleDown}/>
+      <br></br>
+      <Triangle style={styles.triangle}/>
+      <Square style={styles.square}/>
+      <Rectangle style={styles.rectangleDown}/>
+      <br></br>
+      <Triangle style={styles.triangle}/>
+      <View style={{ flexWrap: 'wrap', alignItems: 'flex-start', flexDirection: 'row',}}>
+        <Triangle style={styles.triangleLeft}/>
+        <Square style={styles.square}/>
+        <Triangle style={styles.triangleRight}/>
+      </View>
+      <Triangle style={styles.triangleDown}/>
+      <br></br>
+      <View style={{ flexWrap: 'wrap', alignItems: 'flex-start', flexDirection: 'row',}}>
+        <Circle style={styles.circleRed}></Circle>
+        <Circle style={styles.circleRed}></Circle>
+        <Circle style={styles.circleRed}></Circle>
+      </View>
+      <View style={{ flexWrap: 'wrap', alignItems: 'flex-start', flexDirection: 'row',}}>
+        <Circle style={styles.circleRed}></Circle>
+        <Circle style={styles.circleBlue}></Circle>
+        <Circle style={styles.circleRed}></Circle>
+      </View>
+      <View style={{ flexWrap: 'wrap', alignItems: 'flex-start', flexDirection: 'row',}}>
+        <Circle style={styles.circleRed}></Circle>
+        <Circle style={styles.circleRed}></Circle>
+        <Circle style={styles.circleRed}></Circle>
+      </View>
+      <br></br>
+      <View style={{ flexWrap: 'wrap', alignItems: 'flex-start', flexDirection: 'row',}}>
+        <QuarterCircle style={styles.degreeUpL}></QuarterCircle>
+        <QuarterCircle style={styles.degreeUpR}></QuarterCircle>
+      </View>
+      <View style={{ flexWrap: 'wrap', alignItems: 'flex-start', flexDirection: 'row',}}>
+        <QuarterCircle style={styles.degreeDownL}></QuarterCircle>
+        <QuarterCircle style={styles.degreeDownR}></QuarterCircle>
+      </View>
+
     </View>
   );
 }
@@ -17,6 +62,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  circleRed: {
+    width: 100,
+    height: 100,
+    borderRadius: 100/2,
+    backgroundColor: 'red',
+  },
+  circleBlue: {
+    width: 100,
+    height: 100,
+    borderRadius: 100/2,
+    backgroundColor: 'blue',
+  },
   square: {
     width: 100,
     height: 100,
@@ -26,5 +83,106 @@ const styles = StyleSheet.create({
     width: 200,
     height: 200,
     backgroundColor: 'red',
+  },
+  rectangleUp: {
+    width: 200,
+    height: 100,
+    backgroundColor: 'red'
+  },
+  rectangleDown: {
+    width: 200,
+    height: 100,
+    backgroundColor: 'blue'
+  },
+  squareRight: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'red'
+  },
+  squareLeft:{
+    height: 100,
+    width: 100,
+    backgroundColor: 'blue'
+  },
+  triangle: {
+    width: 0,
+    height: 0,
+    borderLeftWidth: 50,
+    borderRightWidth: 50,
+    borderBottomWidth: 100,
+    borderStyle: 'solid',
+    backgroundColor: 'transparent',
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'blue'
+  },
+  triangleLeft: {
+    width: 0,
+    height: 0,
+    transform: [
+      { rotateZ: "270deg" }
+    ],
+    borderLeftWidth: 50,
+    borderRightWidth: 50,
+    borderBottomWidth: 100,
+    borderStyle: 'solid',
+    backgroundColor: 'transparent',
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'blue'
+  },
+  triangleRight: {
+    width: 0,
+    height: 0,
+    transform: [
+      { rotateZ: "90deg" }
+    ],
+    borderLeftWidth: 50,
+    borderRightWidth: 50,
+    borderBottomWidth: 100,
+    borderStyle: 'solid',
+    backgroundColor: 'transparent',
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'blue'
+  },
+  triangleDown: {
+    width: 0,
+    height: 0,
+    transform: [
+      { rotateZ: "180deg" }
+    ],
+    borderLeftWidth: 50,
+    borderRightWidth: 50,
+    borderBottomWidth: 100,
+    borderStyle: 'solid',
+    backgroundColor: 'transparent',
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'blue'
+  },
+  degreeUpL: {
+    width: 100,
+    height: 100,
+    borderTopLeftRadius: 100,
+    backgroundColor: 'blue'
+  },
+  degreeUpR: {
+    width: 100,
+    height: 100,
+    borderTopRightRadius: 100,
+    backgroundColor: 'red'
+  },
+  degreeDownL: {
+    width: 100,
+    height: 100,
+    borderBottomLeftRadius: 100,
+    backgroundColor: 'red'
+  },
+  degreeDownR: {
+    width: 100,
+    height: 100,
+    borderBottomRightRadius: 100,
+    backgroundColor: 'blue'
   }
 });
