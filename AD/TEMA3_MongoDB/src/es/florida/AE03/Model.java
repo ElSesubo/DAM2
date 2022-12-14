68156f7fb43c67f6e65bd5b5d9df790496e71f00
ElSesubo <gaarag@floridauniversitaria.es>
2022-12-14 20:26:26 +0100

    update

--- AD/TEMA3_MongoDB/src/es/florida/AE03/Model.java
+++ AD/TEMA3_MongoDB/src/es/florida/AE03/Model.java
@@ -1,10 +1,18 @@
 package es.florida.AE03;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.json.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
@@ -16,22 +24,40 @@ public class Model {
 	static MongoClient mongoClient;
 	static MongoDatabase database;
 	static MongoCollection<Document> coleccion;
-	
-	public Model(MongoClient mongoClient, MongoDatabase database, MongoCollection<Document> coleccion) {
-		this.mongoClient = mongoClient;
-		this.database = database;
-		this.coleccion = coleccion;
-	}
+	static ArrayList<MongoCollection> colecciones;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 	}
 	
+	public void conexion() throws IOException, ParseException {
+		JSONParser parser = new JSONParser();
+        FileReader reader = new FileReader("./conexion.json");
+        Object obj = parser.parse(reader);
+        JSONObject pJsonObj = (JSONObject)obj;
+            
+        String ip = (String) pJsonObj.get("ip");
+        Long port = (Long) pJsonObj.get("port");
+        String databaseS = (String) pJsonObj.get("database");
+        JSONArray collection = (JSONArray)pJsonObj.get("collections");
+        
+        int portR = port.intValue();
+        
+    	mongoClient = new MongoClient(ip, portR);
+    	database = mongoClient.getDatabase(databaseS);
+        for(int i = 0; i < collection.size(); i++) {
+        	JSONObject coleccioneSel = (JSONObject)collection.get(i);
+        	String nameColeccion = (String) coleccioneSel.get("collection");
+        	coleccion = database.getCollection(nameColeccion);
+        	colecciones.add(coleccion);
+        }
+	}
+	
 	public String mostrarInfoId(String id) {
 		String info = "";
 		Bson query = Filters.eq("Id", id);
-		MongoCursor<Document> cursor = coleccion.find(query).iterator();
+		MongoCursor<Document> cursor = colecciones.get(0).find(query).iterator();
         while (cursor.hasNext()) {
         	JSONObject obj = new JSONObject(cursor.next().toJson());
         	info += (obj.getString("Id") + ". " + obj.getString("Titol") + " " + obj.getString("Autor") + " " + 
@@ -53,6 +79,22 @@ public class Model {
 	
 	public void insertObject(Document doc) {
 		coleccion.insertOne(doc);
+	}
+	
+	public boolean login(String user, String password) {
+		boolean continuar = false;
+		String pass256 = DigestUtils.sha256Hex(password);
+		String us = null, pass = null;
+		MongoCursor<Document> cursor = colecciones.get(1).find().iterator();
+        while (cursor.hasNext()) {
+        	JSONObject obj = new JSONObject(cursor.next().toJson());
+        	us = obj.getString("user");
+        	pass = obj.getString("pass");
+        }
+        if(user.equals(us) && pass256.equals(pass)) {
+        	continuar = true;
+        }
+        return continuar;
 	}
 
 }
--- AD/TEMA3_MongoDB/src/es/florida/AE03/Principal.java
+++ AD/TEMA3_MongoDB/src/es/florida/AE03/Principal.java
@@ -1,26 +1,26 @@
 package es.florida.AE03;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.bson.Document;
-import org.json.JSONObject;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class Principal {
-	static MongoClient mongoClient = new MongoClient("localhost", 27017);
-	static MongoDatabase database = mongoClient.getDatabase("damMongoDB");
-	static MongoCollection<Document> coleccion = database.getCollection("Llibres");
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ParseException {
 		// TODO Auto-generated method stub
-		JSONObject file = new JSONObject();
-		 
-	    file.put("Full Name", "Ritu Sharma");
-	    file.put("Roll No.", new Integer(1704310046));
-	    file.put("Tuition Fees", new Double(65400));
 	    
-		Model model = new Model(mongoClient, database, coleccion);
+		Model model = new Model();
 		Vista vista = new Vista();
 
 		Controlador controlador = new Controlador(vista, model);
